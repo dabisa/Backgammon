87889a068b44918fe2a24f9b6d2eb28dbf1b5fa9
Dabisa Kelava <dabisa.kelava@gmail.com>
2017-08-11 03:33:21 +0100

    added backgammon web server application

---
+++ websrv/src/main/java/com/dkelava/backgammon/websrv/BackgammonWebServer.java
@@ -0,0 +1,12 @@
+package com.dkelava.backgammon.websrv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackgammonWebServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackgammonWebServer.class, args);
+	}
+}
